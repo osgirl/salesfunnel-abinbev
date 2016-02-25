29fa5a8f64b59d3b229043da62c77d8ebf694212
easybird <easybird85@gmail.com>
2016-02-25 14:27:30 +0100

    TG-12 add materialise css + inbev banner

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index',{
+          metaData: {
+              title: 'Sales funnel - reporting tool - AB Inbev',
+              description: 'This is a reporting tool by and from AB Inbev to report about sales prospects'
+          }
+      }
+  );
 });
 
 module.exports = router;
