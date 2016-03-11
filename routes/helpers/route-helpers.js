7dd04c442f0f238c6c4acb303c1ca441e7f812f1
easybird <easybird85@gmail.com>
2016-03-11 22:58:46 +0100

    quick fix https..

--- routes/helpers/route-helpers.js
+++ routes/helpers/route-helpers.js
@@ -1,8 +1,9 @@
 import url from 'url';
 
 export function getBaseUrl(req) {
+    //TODO fix hardcoded https with secure connection
     return url.format({
-        protocol: req.protocol,
+        protocol: 'https',
         host: req.get('host'),
         pathname: '/'
     });
