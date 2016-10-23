176eef48404fb780f0dc8461a9671891ea722e4b
jonathan dierckens <jonathan.dierckens@persgroep.be>
2016-10-23 19:54:47 +0200

    remove migrations...

--- migrations/1477119081192-emailToLowercase.js
+++ migrations/1477119081192-emailToLowercase.js
@@ -1,14 +1,7 @@
-import User from '../model/users/user-schema';
-
 exports.up = function (next) {
-    return User.find({}, function (err, users) {
-        if (err) return next(err);
-        return Promise.all(users.map((user) => User.findOneAndUpdate({ _id: user._id })))
-            .then(next)
-            .catch(next);
-    });
+    return next();
 };
 
 exports.down = function (next) {
-    next();
+    return next();
 };
