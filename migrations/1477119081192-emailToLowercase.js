afb90da41e1f9d15dce22c7a24ac24a4050806b0
jonathan dierckens <jonathan.dierckens@persgroep.be>
2016-10-23 19:36:57 +0200

    fix then/catch promise

--- migrations/1477119081192-emailToLowercase.js
+++ migrations/1477119081192-emailToLowercase.js
@@ -1,14 +1,10 @@
 import User from '../model/users/user-schema';
-import _ from 'lodash';
 
 exports.up = function (next) {
     return User.find({}, function (err, users) {
-        if (err) return next();
-        return _(users).forEach((user) => {
-            // lowercase will be set by the adaptation in the scheme itself
-            return User.findOneAndUpdate({ _id: user._id });
-        })
-            .then(() => next())
+        if (err) return next(err);
+        return Promise.all(users.map((user) => User.findOneAndUpdate({ _id: user._id })))
+            .then(next)
             .catch(next);
     });
 };
